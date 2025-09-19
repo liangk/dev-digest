7d466fb61c6648be25a60fa6edd69bef6964f58d
liangk <liangk.tw@gmail.com>
2025-09-20 09:28:09 +1000

    Fix losing navigate issue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { BlogService } from '../services/blog.service';
 import { BlogPost } from '../models/base';
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, RouterModule],
   providers: [BlogService],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
   posts: BlogPost[] = [];
-  constructor(private blogService: BlogService) {
+
+  constructor(private blogService: BlogService, private router: Router) {
     this.blogService.getPostMetadata().subscribe((posts: BlogPost[]) => {
       this.posts = posts;
     });
@@ -21,9 +23,14 @@ export class HomeComponent {
 
   ngOnInit() {
   }
+
   gotoRepo(event: any, repo: string) {
     event.preventDefault();
     event.stopPropagation();
     window.open(repo, '_blank')
+  }
+
+  navigateToPost(slug: string): void {
+    this.router.navigate(['/blog', slug]);
   }
 }
